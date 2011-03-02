a69327ab5e5d42986ecbd38d56bb4596b2d8a70d
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-03-02 01:49:26 -0500

    Added a noend option, if that is set, the request will not be automatically ended for get requests, let alone any other type of request.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- http_compat.js
+++ http_compat.js
@@ -1,18 +1,17 @@
 var http = require('http'),
     version = process.version;
-var exports = module.exports;
 
 if (version.indexOf('v0.2') >= 0) {
-    exports.request = function(opts, cb) {
+    module.exports.request = function(opts, cb) {
         var client = http.createClient(opts.port, opts.host),
             headers = {host: opts.host+((parseInt(opts.port, 10) !== 80)?':'+opts.port:'')},
             req = client.request(opts.method, opts.path, headers);
         req.on('response', cb);
-        if (opts.method.match(/get/i)) {
+        if (opts.noend || (!(opts.noend) && opts.method.match(/get/i))) {
             req.end();
         }
         return req;
     }
 } else {
-    exports = http;
+    module.exports = http;
 }
